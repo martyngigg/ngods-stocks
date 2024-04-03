0dc12511ca9b5ace39bc6899292c09c728b06c25
Martyn Gigg <martyn.gigg@gmail.com>
2024-04-03 13:05:14 +0100

    Rename dbt.py to avoid a circular import issue
    The dagster project directory is set as the working
    directory for loading scripts and autoloads all .py scripts.
    Importing "dagster_dbt import dbt_cli_resource" inside dbt.py
    causes a circular reference as an internal import tries to
    do "from dbt.contracts..." and then attempts to import dbt.py
    again.
    Renaming is the simplest way to avoid this.

--- projects/dagster/dbtjobs.py
+++ projects/dagster/dbtjobs.py
@@ -7,7 +7,7 @@ def _dbt_run(context):
     pdir = context.op_config['project_dir']
     context.log.info(f"elt: executing dbt run with project_dir: '{pdir}'")
     if 'select' in context.op_config:
-        s = context.op_config['select']        
+        s = context.op_config['select']
         context.log.info(f"elt: executing dbt run with select: '{s}'")
         context.resources.dbt.cli("run", project_dir=pdir, select=s)
     else:
@@ -17,7 +17,7 @@ def _dbt_test(context):
     pdir = context.op_config['project_dir']
     context.log.info(f"elt: executing dbt test with project_dir: '{pdir}'")
     if 'select' in context.op_config:
-        s = context.op_config['select']        
+        s = context.op_config['select']
         context.log.info(f"elt: executing dbt run with select: '{s}'")
         context.resources.dbt.cli("test", project_dir=pdir, select=s)
     else:
@@ -27,7 +27,7 @@ def _dbt_generate_docs(context):
     pdir = context.op_config['project_dir']
     context.log.info(f"elt: executing dbt docs generate with project_dir: '{pdir}'")
     if 'select' in context.op_config:
-        s = context.op_config['select']        
+        s = context.op_config['select']
         context.log.info(f"elt: executing dbt run with select: '{s}'")
         context.resources.dbt.cli("docs generate", project_dir=pdir, select=s)
     else:
@@ -37,7 +37,7 @@ def _dbt_source_freshness(context):
     pdir = context.op_config['project_dir']
     context.log.info(f"elt: executing dbt source freshness with project_dir: '{pdir}'")
     if 'select' in context.op_config:
-        s = context.op_config['select']        
+        s = context.op_config['select']
         context.log.info(f"elt: executing dbt run with select: '{s}'")
         context.resources.dbt.cli("source freshness", project_dir=pdir, select=s)
     else:
@@ -47,7 +47,7 @@ def _dbt_source_freshness(context):
 @op(required_resource_keys={'dbt'})
 def dbt_bronze_run_op(context, dependent_job=None):
     _dbt_run(context)
-    
+
 @op(required_resource_keys={'dbt'})
 def dbt_bronze_test_doc_sources_op(context, dependent_job=None):
     _dbt_test(context)
@@ -83,15 +83,15 @@ def dbt_bronze():
 
 
 @job(resource_defs={'dbt': dbt_cli_resource})
-def dbt_silver():    
+def dbt_silver():
     dbt_silver_test_doc_sources_op(dbt_silver_run_op())
 
 @job(resource_defs={'dbt': dbt_cli_resource})
-def dbt_gold():    
+def dbt_gold():
     dbt_gold_test_doc_sources_op(dbt_gold_run_op())
 
 @job(resource_defs={'dbt': dbt_cli_resource})
-def dbt_all():    
+def dbt_all():
     dbt_gold_test_doc_sources_op(
         dbt_gold_run_op(
             dbt_silver_test_doc_sources_op(
